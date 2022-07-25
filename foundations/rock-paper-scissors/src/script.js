f8d6242883d8f71e8d7f6c933ba3384bf0568809
Mark P <mj.poso18@gmail.com>
2022-07-26 03:40:29 +0800

    Create required `getComputerChoice()` function

--- foundations/rock-paper-scissors/src/script.js
+++ foundations/rock-paper-scissors/src/script.js
@@ -1,12 +1,22 @@
 import utils from './utils.js';
 
-const { getRandomInteger, getRandomElement } = utils;
+const CHOICES = ['Rock', 'Paper', 'Scissors'];
+
+function getComputerChoice() {
+  const { getRandomElement } = utils;
+  const computerChoice = getRandomElement(CHOICES);
+
+  return computerChoice;
+}
 
 /*  */
+
+const { getRandomInteger, getRandomElement } = utils;
 
 let _ = [
   /*  */
   getRandomInteger({ lower: 0, upper: 100 }),
   getRandomElement(['Rock', 'Paper', 'Scissors']),
+  getComputerChoice(),
 ];
 console.log(_);
