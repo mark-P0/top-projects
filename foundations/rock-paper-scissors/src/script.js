2c4448ff518f65a93174de9a0675ad020ceb0c31
Mark P <mj.poso18@gmail.com>
2022-07-26 04:03:08 +0800

    Initialize main `game()` function

--- foundations/rock-paper-scissors/src/script.js
+++ foundations/rock-paper-scissors/src/script.js
@@ -2,11 +2,24 @@ import utils from './utils.js';
 
 const CHOICES = ['Rock', 'Paper', 'Scissors'];
 
+const PARTICIPANT = {
+  Player: 'Player',
+  Computer: 'Computer',
+  NONE: undefined,
+};
+
 function getComputerChoice() {
   const { getRandomElement } = utils;
   const computerChoice = getRandomElement(CHOICES);
 
   return computerChoice;
+}
+
+function getPlayerChoice() {
+  const result = getComputerChoice();
+  console.warn('Player choice still randomized!');
+
+  return result;
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -22,24 +35,64 @@ function playRound(playerSelection, computerSelection) {
    */
   const playerSelIdxNext = (playerSelIdx + 1) % CHOICES.length;
 
+  /* prettier-ignore */
   const result =
     playerSelIdx === computerSelIdx
-      ? `It's a tie! You both played ${playerSelection}`
+      ? [`It's a tie! You both played ${playerSelection}`, PARTICIPANT.NONE]
       : playerSelIdxNext === computerSelIdx
-      ? `You Lose! ${computerSelection} beats ${playerSelection}`
-      : `You Win! ${playerSelection} beats ${computerSelection}`;
+      ? [`You Lose! ${computerSelection} beats ${playerSelection}`, PARTICIPANT.Computer]
+      : [`You Win! ${playerSelection} beats ${computerSelection}`, PARTICIPANT.Player]
 
   return result;
 }
 
-/*  */
+function game(rounds = 5) {
+  const scores = {
+    [PARTICIPANT.Player]: 0,
+    [PARTICIPANT.Computer]: 0,
+  };
 
-for (const playerChoice of CHOICES) {
-  for (const computerChoice of CHOICES) {
-    console.log([
-      playerChoice,
-      computerChoice,
-      playRound(playerChoice, computerChoice),
-    ]);
+  console.log(`Rock-Paper-Scissors! A game of ${rounds} will be played.`);
+  console.log('');
+
+  const roundCts = Array.from({ length: rounds }, (_, idx) => idx + 1);
+  for (const roundCt of roundCts) {
+    console.log(`Round ${roundCt}:`);
+
+    console.log('Getting player input...');
+    const playerChoice = getPlayerChoice();
+    const computerChoice = getComputerChoice();
+
+    console.log(
+      [
+        `Your play:       ${playerChoice}`,
+        `Computer's play: ${computerChoice}`,
+      ].join('\n')
+    );
+
+    const [resultMsg, roundWinner] = playRound(playerChoice, computerChoice);
+
+    console.log(resultMsg);
+    console.log('');
+
+    if (roundWinner === PARTICIPANT.NONE) continue;
+    scores[roundWinner] += 1;
   }
+
+  const {
+    [PARTICIPANT.Player]: playerScore,
+    [PARTICIPANT.Computer]: computerScore,
+  } = scores;
+
+  const finalMsg =
+    playerScore === computerScore
+      ? 'The game is a tie!'
+      : playerScore < computerScore
+      ? ['The computer won the game!', 'You lost!'].join('\n')
+      : 'You won the game! Congratulations!!!';
+
+  console.log(finalMsg);
+  console.log(scores);
 }
+
+game();
