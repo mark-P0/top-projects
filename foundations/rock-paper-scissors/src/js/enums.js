97a1feaf51f1f9ddbce74b4478c4d96893d0dc2c
Mark P <mj.poso18@gmail.com>
2022-07-29 15:58:37 +0800

    Move enums to separate file and refactor script accordingly

---
+++ foundations/rock-paper-scissors/src/js/enums.js
@@ -0,0 +1,15 @@
+const enums = {
+  Choices: {
+    ROCK: '✊🏼',
+    PAPER: '🖐🏼',
+    SCISSORS: '✌🏼',
+  },
+
+  Participant: {
+    PLAYER: 'Player',
+    COMPUTER: 'Computer',
+    NONE: undefined,
+  },
+};
+
+export default enums;
--- foundations/rock-paper-scissors/src/js/script.js
+++ foundations/rock-paper-scissors/src/js/script.js
@@ -1,40 +1,37 @@
+import enums from './enums.js';
 import utils from './utils.js';
 
-const CHOICES = ['Rock', 'Paper', 'Scissors'];
-
-const PARTICIPANT = {
-  Player: 'Player',
-  Computer: 'Computer',
-  NONE: undefined,
-};
+const { Choices, Participant } = enums;
 
 function getComputerChoice() {
   const { getRandomElement } = utils;
-  const computerChoice = getRandomElement(CHOICES);
+  const computerChoiceKey = getRandomElement(Object.keys(Choices));
+  const computerChoice = Choices[computerChoiceKey];
 
   return computerChoice;
 }
+console.log(getComputerChoice());
 
 function playRound(playerSelection, computerSelection) {
-  const playerSelIdx = CHOICES.indexOf(playerSelection);
-  const computerSelIdx = CHOICES.indexOf(computerSelection);
+  const playerSelIdx = Choices.indexOf(playerSelection);
+  const computerSelIdx = Choices.indexOf(computerSelection);
 
   /*  The game choices' relationships are such that,
-      given `choice` as `CHOICES[idx]`,
-      `choice` is beaten by `CHOICES[(idx + 1) % CHOICES.length]`
+      given `choice` as `Choices[idx]`,
+      `choice` is beaten by `Choices[(idx + 1) % Choices.length]`
       i.e. the choice "next" to them
 
       `playerSelIdxNext` will be used to compare to `computerSelIdx`
    */
-  const playerSelIdxNext = (playerSelIdx + 1) % CHOICES.length;
+  const playerSelIdxNext = (playerSelIdx + 1) % Choices.length;
 
   /* prettier-ignore */
   const result =
     playerSelIdx === computerSelIdx
-      ? [`It's a tie! You both played ${playerSelection}`, PARTICIPANT.NONE]
+      ? [`It's a tie! You both played ${playerSelection}`, Participant.NONE]
       : playerSelIdxNext === computerSelIdx
-      ? [`You Lose! ${computerSelection} beats ${playerSelection}`, PARTICIPANT.Computer]
-      : [`You Win! ${playerSelection} beats ${computerSelection}`, PARTICIPANT.Player]
+      ? [`You Lose! ${computerSelection} beats ${playerSelection}`, Participant.COMPUTER]
+      : [`You Win! ${playerSelection} beats ${computerSelection}`, Participant.PLAYER]
 
   return result;
 }
