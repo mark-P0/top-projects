9cd09d8a65297b4ffc439578f3f7e947aaa56541
Mark P <mj.poso18@gmail.com>
2022-07-29 15:48:21 +0800

    Remove game loop and user input functions

--- foundations/rock-paper-scissors/src/js/script.js
+++ foundations/rock-paper-scissors/src/js/script.js
@@ -15,32 +15,6 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-function getPlayerChoice(
-  promptMsgDefault = 'Rock, Paper, or Scissors?\n[See console for game output]'
-) {
-  const { capitalizeString } = utils;
-
-  let userInput = '';
-  let userInputCapitalized = '';
-  let promptMsg = promptMsgDefault;
-
-  while (true) {
-    userInput = prompt(promptMsg);
-    userInputCapitalized = capitalizeString(userInput);
-
-    if (CHOICES.includes(userInputCapitalized)) break;
-
-    /* prettier-ignore */
-    promptMsg = [
-      `Input "${userInput}" is invalid!`,
-      promptMsgDefault,
-    ].join('\n\n')
-  }
-
-  /* Use enum version of `CHOICES` instead? */
-  return userInputCapitalized;
-}
-
 function playRound(playerSelection, computerSelection) {
   const playerSelIdx = CHOICES.indexOf(playerSelection);
   const computerSelIdx = CHOICES.indexOf(computerSelection);
@@ -64,57 +38,6 @@ function playRound(playerSelection, computerSelection) {
 
   return result;
 }
-
-function game(rounds = 5) {
-  const scores = {
-    [PARTICIPANT.Player]: 0,
-    [PARTICIPANT.Computer]: 0,
-  };
-
-  console.log(`Rock-Paper-Scissors! A game of ${rounds} will be played.`);
-  console.log('');
-
-  const roundCts = Array.from({ length: rounds }, (_, idx) => idx + 1);
-  for (const roundCt of roundCts) {
-    console.log(`Round ${roundCt}:`);
-
-    console.log('Getting player input...');
-    const playerChoice = getPlayerChoice();
-    const computerChoice = getComputerChoice();
-
-    const choicesFeedback = [
-      `Your play:       ${playerChoice}`,
-      `Computer's play: ${computerChoice}`,
-    ].join('\n');
-    console.log(choicesFeedback);
-
-    const [resultMsg, roundWinner] = playRound(playerChoice, computerChoice);
-
-    console.log(resultMsg);
-    console.log('');
-
-    if (roundWinner === PARTICIPANT.NONE) continue;
-    scores[roundWinner] += 1;
-  }
-
-  const {
-    [PARTICIPANT.Player]: playerScore,
-    [PARTICIPANT.Computer]: computerScore,
-  } = scores;
-
-  const finalMsg =
-    playerScore === computerScore
-      ? 'The game is a tie!'
-      : playerScore < computerScore
-      ? ['The computer won the game!', 'You lost!'].join('\n')
-      : 'You won the game! Congratulations!!!';
-
-  console.log(finalMsg);
-  console.log(scores);
-}
-
-console.warn('Console game is disabled! Rounds set to 0');
-game(0);
 
 /*  */
 
