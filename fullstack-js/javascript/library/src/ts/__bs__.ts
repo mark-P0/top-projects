7f03aee3a31dd251e37b80c5c0964c5ac8c33686
Mark P <mj.poso18@gmail.com>
2022-09-28 03:52:48 +0800

    Move Bootstrap requisite script into its own file

---
+++ fullstack-js/javascript/library/src/ts/__bs__.ts
@@ -0,0 +1,44 @@
+/*  Enable form validation powered by Bootstrap
+ *  As officially exampled:
+ *  https://getbootstrap.com/docs/5.2/forms/validation/
+ */
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+function bsEnableFormValidation() {
+  'use strict';
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(
+    '.needs-validation'
+  ) as NodeListOf<HTMLFormElement>;
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      'submit',
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add('was-validated');
+      },
+      false
+    );
+  });
+}
+bsEnableFormValidation();
+
+/*  Reset modal form on hide
+ *  https://getbootstrap.com/docs/5.2/components/modal/#events
+ */
+function bsResetModalFormOnHide() {
+  const modal = document.querySelector('.modal');
+  const form = document.querySelector('.modal form') as HTMLFormElement;
+
+  modal.addEventListener('hidden.bs.modal', () => {
+    form.reset();
+    form.classList.remove('was-validated');
+  });
+}
+bsResetModalFormOnHide();
--- fullstack-js/javascript/library/src/ts/app.ts
+++ fullstack-js/javascript/library/src/ts/app.ts
@@ -1,3 +1,4 @@
+import './__bs__.js';
 import './book.js';
 import './library.js';
 import './form.js';
