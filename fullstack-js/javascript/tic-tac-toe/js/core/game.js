645a22f79e7bc46d7ba67cf9ad77dc697773581c
Mark P <mj.poso18@gmail.com>
2022-10-02 20:32:08 +0800

    Move game grid as a standalone object

--- fullstack-js/javascript/tic-tac-toe/js/core/game.js
+++ fullstack-js/javascript/tic-tac-toe/js/core/game.js
@@ -1,41 +1,61 @@
-import { getChoices, getDivMod } from '../utils.js';
+import { getChoices, getDivMod, transpose } from '../utils.js';
 import { PlayerFactory } from './player.js';
+
+const Marks = {
+  X: '❌',
+  O: '⭕',
+  _: '', // Unmarked cell
+};
+
+const Grid = (() => {
+  const size = 3;
+  let grid = Array.from({ length: size }, () => Array(size).fill(Marks._));
+
+  const markCell = (coords, mark) => {
+    const [x, y] = coords;
+    grid[y][x] = mark;
+  };
+
+  return {
+    size,
+    get rows() {
+      return grid;
+    },
+    get columns() {
+      return transpose(grid);
+    },
+
+    markCell,
+  };
+})();
 
 const Game = (() => {
   /* Holder for private variables; really unnecessary, but arguably makes code more clean */
   const __ = {};
-
-  const marks = {
-    X: '❌',
-    O: '⭕',
-    _: '', // Unmarked cell
-  };
-  __.gridSize = 3;
-  const grid = Array.from({ length: __.gridSize }, () =>
-    Array(__.gridSize).fill(marks._)
-  );
-  const title = getChoices(Object.values(marks).slice(0, -1), 3).join('');
+  const title = getChoices(Object.values(Marks).slice(0, -1), 3).join('');
 
   /* Player-related code */
-  const players = Object.values(marks)
+  const players = Object.values(Marks)
     .slice(0, -1)
     .map((mark) => PlayerFactory.create(null, mark));
   __.currentPlayerIdx = 0;
   const makeMove = (x, y) => {
     const { mark } = players[__.currentPlayerIdx];
-    grid[y][x] = mark;
+    Grid.markCell([x, y], mark);
 
     __.currentPlayerIdx = (__.currentPlayerIdx + 1) % players.length;
 
     return mark;
   };
   const transformIndexToCoords = (idx) => {
-    return getDivMod(idx, __.gridSize);
+    return getDivMod(idx, Grid.size);
   };
 
   return {
-    marks,
-    grid,
+    marks: Marks,
+    get grid() {
+      return Grid.rows;
+    },
     title,
 
     players,
--- fullstack-js/javascript/tic-tac-toe/js/utils.js
+++ fullstack-js/javascript/tic-tac-toe/js/utils.js
@@ -24,4 +24,17 @@ function getDivMod(n, d) {
   return [getIntDiv(n, d), n % d];
 }
 
-export { getChoices, getDivMod };
+function transpose(arr) {
+  const columnCt = Math.max(...arr.map((row) => row.length));
+
+  const transposed = Array.from({ length: columnCt }, () => []);
+  for (const row of arr) {
+    for (let idx = 0; idx < columnCt; idx++) {
+      transposed[idx].push(row[idx] ?? null);
+    }
+  }
+
+  return transposed;
+}
+
+export { getChoices, getDivMod, transpose };
