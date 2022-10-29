4422c5a011e9c1ea106784463c33ba9711867ee4
Mark P <mj.poso18@gmail.com>
2022-10-29 19:55:19 +0800

    Use HTTPS for external resources

--- fullstack-js/javascript/restaurant-page/script.js
+++ fullstack-js/javascript/restaurant-page/script.js
@@ -1,9 +1,7 @@
 const contentNav = document.getElementById('content-nav');
 contentNav.addEventListener('click', ({ target }) => {
-  console.log(target);
   if (!target.classList.contains('btn-check')) return;
   contentNav.dataset.nav = target.id;
-  console.log(target);
 });
 contentNav
   .querySelector('[checked]')
