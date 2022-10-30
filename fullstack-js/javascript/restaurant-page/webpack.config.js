452445037bc7987a55b5f863f38823cdf29a759c
Mark P <mj.poso18@gmail.com>
2022-10-30 19:04:12 +0800

    Set Webpack to use ESM

--- fullstack-js/javascript/restaurant-page/webpack.config.js
+++ fullstack-js/javascript/restaurant-page/webpack.config.js
@@ -1,13 +1,22 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+/*  Use ES6 syntax
+ *  So simple in hindsight...
+ *  https://stackoverflow.com/a/71626398/
+ */
 
-module.exports = {
+import path from 'node:path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+export default {
   /* Essentials */
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    /*  The official docs also uses `process.cwd()`
+     *  in lieu of CommonJS global `__dirname`
+     *  https://webpack.js.org/configuration/output/#outputpath
+     */
+    path: path.resolve(process.cwd(), 'dist'),
     clean: true,
   },
 
@@ -27,6 +36,19 @@ module.exports = {
       {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+
+      /*  For ES6 Modules (ESM). Allows...
+       *  - Importing without specifying the file extension
+       *  - Importing of "directories" (automatically resolves to inner `index.js`)
+       *    - This behavior can be customized with the `resolve.mainFiles` setting
+       *    - https://webpack.js.org/configuration/resolve/#resolvemainfiles
+       *
+       *  https://webpack.js.org/configuration/module/#resolvefullyspecified
+       */
+      {
+        test: /\.m?js$/,
+        resolve: { fullySpecified: false },
       },
     ],
   },
