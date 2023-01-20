32da71d7e8859089584406e4bc62bd4b2c262e7c
Mark P <mj.poso18@gmail.com>
2023-01-20 18:52:54 +0800

    Fix return type hint of string identifier

--- fullstack-js/javascript/testing-practice/src/utilities.js
+++ fullstack-js/javascript/testing-practice/src/utilities.js
@@ -1,6 +1,6 @@
 /**
  * https://stackoverflow.com/a/9436948/
- * @type {(str: string) => string}
+ * @type {(str: string) => boolean}
  */
 export function isString(str) {
   return typeof str === 'string' || str instanceof String;
