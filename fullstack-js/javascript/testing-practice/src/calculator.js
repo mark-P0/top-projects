fe16fa94e58201f657c08631f2b2ceaa60261ce8
Mark P <mj.poso18@gmail.com>
2023-01-24 22:54:56 +0800

    Implement calculator `division` function

--- fullstack-js/javascript/testing-practice/src/calculator.js
+++ fullstack-js/javascript/testing-practice/src/calculator.js
@@ -22,7 +22,16 @@ class Calculator {
     return a * b;
   }
 
-  divide(a, b) {}
+  /** @type {(a: number, b: number) => number} */
+  divide(a, b) {
+    if (!isNumber(a)) throw new Error(`Given \`${a}\` is not a number!`);
+    if (!isNumber(b)) throw new Error(`Given \`${b}\` is not a number!`);
+    if (a === b) return 1;
+    if (a === 0) return 0;
+    if (b === 0) throw new Error(`Division by zero not possible!`);
+    if (b === 1) return a;
+    return a / b;
+  }
 }
 
 export const calculator = new Calculator();
--- fullstack-js/javascript/testing-practice/tests/calculator.test.js
+++ fullstack-js/javascript/testing-practice/tests/calculator.test.js
@@ -249,3 +249,109 @@ describe('Behavior of `multiply` operation', () => {
     expect(calculator.multiply(-1.1, 2.2, -3.3)).toBeCloseTo(-2.42);
   });
 });
+
+describe('Behavior of `divide` operation', () => {
+  test('Divides 2 numbers', () => {
+    expect(calculator.divide(-1, -2)).toBeCloseTo(0.5);
+    expect(calculator.divide(1, 2)).toBeCloseTo(0.5);
+    expect(calculator.divide(1, -2)).toBeCloseTo(-0.5);
+    expect(calculator.divide(-1, 2)).toBeCloseTo(-0.5);
+    expect(calculator.divide(-1.1, -2.2)).toBeCloseTo(0.5);
+    expect(calculator.divide(0.3, 0.4)).toBeCloseTo(0.75);
+    expect(calculator.divide(1.5, 2.6)).toBeCloseTo(0.5769);
+  });
+
+  test('Same dividend and divisor results to 1', () => {
+    expect(calculator.divide(2, 2)).toBe(1);
+    expect(calculator.divide(-3, -3)).toBe(1);
+    expect(calculator.divide(4.1, 4.1)).toBe(1);
+    expect(calculator.divide(-5.2, -5.2)).toBe(1);
+  });
+
+  test('Dividing 0 by anything results to 0', () => {
+    expect(calculator.divide(0, 2)).toBe(0);
+    expect(calculator.divide(0, -3)).toBe(0);
+    expect(calculator.divide(0, 4.1)).toBe(0);
+    expect(calculator.divide(0, -5.2)).toBe(0);
+  });
+
+  test('Dividing anything by 0 throws an error', () => {
+    expect(() => calculator.divide(2, 0)).toThrow();
+    expect(() => calculator.divide(-3, 0)).toThrow();
+    expect(() => calculator.divide(4.1, 0)).toThrow();
+    expect(() => calculator.divide(-5.2, 0)).toThrow();
+  });
+
+  test('Dividing anything by 1 results to that number', () => {
+    expect(calculator.divide(2, 1)).toBe(2);
+    expect(calculator.divide(-3, 1)).toBe(-3);
+    expect(calculator.divide(4.1, 1)).toBeCloseTo(4.1);
+    expect(calculator.divide(-5.2, 1)).toBeCloseTo(-5.2);
+  });
+
+  describe('Only accepts numbers', () => {
+    test('Accepts integers', () => {
+      expect(() => calculator.divide(-123, -123)).not.toThrow();
+      expect(() => calculator.divide(0, 0)).not.toThrow();
+      expect(() => calculator.divide(123, 123)).not.toThrow();
+      expect(() => calculator.divide(123, -123)).not.toThrow();
+      expect(() => calculator.divide(-123, 123)).not.toThrow();
+    });
+
+    test('Accepts floats', () => {
+      expect(() => calculator.divide(-123.456, -123.456)).not.toThrow();
+      expect(() => calculator.divide(0.0, 0.0)).not.toThrow();
+      expect(() => calculator.divide(123.456, 123.456)).not.toThrow();
+      expect(() => calculator.divide(-123.456, 123.456)).not.toThrow();
+      expect(() => calculator.divide(123.456, -123.456)).not.toThrow();
+    });
+
+    test('Does not accept strings', () => {
+      expect(() => calculator.divide('string', 'another string')).toThrow();
+    });
+
+    test('Does not accept booleans', () => {
+      expect(() => calculator.divide(true, false)).toThrow();
+      expect(() => calculator.divide(false, true)).toThrow();
+    });
+
+    test('Does not accept nullish values', () => {
+      expect(() => calculator.divide(null, undefined)).toThrow();
+      expect(() => calculator.divide(undefined, null)).toThrow();
+      expect(() => calculator.divide()).toThrow(); // Arguments defaults to `undefined`
+    });
+
+    test('Does not accept arrays', () => {
+      expect(() => calculator.divide([], [])).toThrow();
+      expect(() => calculator.divide(['', 0], [false, null])).toThrow();
+    });
+
+    test('Does not accept objects', () => {
+      expect(() => calculator.divide({})).toThrow();
+      expect(() =>
+        calculator.divide({ length: 0 }, { null: undefined })
+      ).toThrow();
+    });
+  });
+
+  test('Must provide 2 numbers', () => {
+    expect(() => calculator.divide(-1)).toThrow();
+    expect(() => calculator.divide(0)).toThrow();
+    expect(() => calculator.divide(1)).toThrow();
+    expect(() => calculator.divide(-1.1)).toThrow();
+    expect(() => calculator.divide(0.2)).toThrow();
+    expect(() => calculator.divide(1.3)).toThrow();
+  });
+
+  test('Only considers 2 numbers', () => {
+    expect(calculator.divide(-1, -2, -3)).toBeCloseTo(0.5);
+    expect(calculator.divide(1, 2, 3)).toBeCloseTo(0.5);
+    expect(calculator.divide(1, -2, 3)).toBeCloseTo(-0.5);
+    expect(calculator.divide(-1, 2, -3)).toBeCloseTo(-0.5);
+    expect(calculator.divide(-1.1, -2.2, -3.3)).toBeCloseTo(0.5);
+    expect(calculator.divide(0.1, 0.2, 0.3)).toBeCloseTo(0.5);
+    expect(calculator.divide(1.1, 2.2, 3.3)).toBeCloseTo(0.5);
+    expect(calculator.divide(1.1, -2.2, 3.3)).toBeCloseTo(-0.5);
+    expect(calculator.divide(-1.1, 2.2, -3.3)).toBeCloseTo(-0.5);
+  });
+});
