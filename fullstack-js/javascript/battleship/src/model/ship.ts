32ab70c8c184ec5698e3e29a3441f5c93366c9d1
Mark P <mj.poso18@gmail.com>
2023-02-07 21:48:14 +0800

    Add braces to error throwing block

--- fullstack-js/javascript/battleship/src/model/ship.ts
+++ fullstack-js/javascript/battleship/src/model/ship.ts
@@ -28,8 +28,9 @@ export abstract class Ship {
    * Increases the number of _hits_ in a ship.
    */
   hit() {
-    if (this.isSunk)
+    if (this.isSunk) {
       throw new ShipHitAfterSinkingError('Ship has already been destroyed!');
+    }
 
     this.#hitCt++;
   }
